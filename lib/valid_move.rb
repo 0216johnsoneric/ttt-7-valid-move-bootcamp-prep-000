6995b1ddfdb0ee3104f6f78debbe522abc199413
Eric Johnson <0216johnsoneric@gmail.com>
2017-10-22 20:31:08 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,8 +2,8 @@
 def valid_move?(board,index)
   if position_taken?(board,index) == false
   return true
-  else position_taken?(board,index) == true
-  return true
+  else 
+  return false
   end
 end
 
